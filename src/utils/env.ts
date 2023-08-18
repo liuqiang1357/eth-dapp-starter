e4b39e2cace747b96072ffbaa15dfdcb1d366bb8
liuqiang <liuqiang1357@163.com>
2023-08-18 14:08:40 +0800

    add visualizer and update scripts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,3 @@
 export const ENV_PRODUCTION = import.meta.env.PROD;
 
-export const TARGET_PRODUCTION = import.meta.env.VITE_APP_TARGET === 'production';
+export const TARGET_PRODUCTION = import.meta.env.MODE === 'production';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    process.env.USE_VISUALIZER === '1' ? visualizer({ open: true, gzipSize: true }) : null,
+  ],
 });
