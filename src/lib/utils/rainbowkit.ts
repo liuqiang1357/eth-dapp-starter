bfeca93fe42b3f948572c7b61220f3a68a13130b
liuqiang <liuqiang1357@163.com>
2024-05-13 13:45:33 +0800

    update rainbowkit theme

--- src/lib/utils/rainbowkit.ts
+++ src/lib/utils/rainbowkit.ts
@@ -1,5 +1,11 @@
 import { darkTheme, lightTheme } from '@rainbow-me/rainbowkit';
 
-export const rainbowkitLightTheme = lightTheme({ accentColor: 'hsl(var(--primary))' });
+export const rainbowkitLightTheme = lightTheme({
+  accentColor: 'hsl(var(--primary))',
+  accentColorForeground: 'hsl(var(--primary-foreground))',
+});
 
-export const rainbowkitDarkTheme = darkTheme({ accentColor: 'hsl(var(--primary))' });
+export const rainbowkitDarkTheme = darkTheme({
+  accentColor: 'hsl(var(--primary))',
+  accentColorForeground: 'hsl(var(--primary-foreground))',
+});
