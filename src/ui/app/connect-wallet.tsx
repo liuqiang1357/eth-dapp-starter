77235579c5fd120508f8b4ccb51d5206436d6675
liuqiang <liuqiang1357@163.com>
2024-11-08 17:15:17 +0800

    refine code

--- src/ui/app/connect-wallet.tsx
+++ src/ui/app/connect-wallet.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
 import { disconnect } from '@wagmi/core';
+import { useAtomValue } from 'jotai';
 import { ComponentProps, FC } from 'react';
+import { accountAtom } from 'lib/states/web3';
 import { formatLongText } from 'lib/utils/formatters';
 import { cn } from 'lib/utils/shadcn';
 import { wagmiConfig } from 'lib/utils/wagmi';
@@ -10,39 +12,31 @@ import { Button } from 'ui/shadcn/button';
 import { AccountIcon } from './account-icon';
 
 export const ConnectWallet: FC<ComponentProps<'div'>> = ({ className, ...props }) => {
-  return (
-    <ConnectButton.Custom>
-      {({ account, authenticationStatus, openConnectModal }) => {
-        const connected =
-          account != null &&
-          (authenticationStatus == null || authenticationStatus === 'authenticated');
+  const account = useAtomValue(accountAtom);
 
-        return (
-          <div className={cn('inline-flex space-x-4', className)} {...props}>
-            {connected ? (
-              <div className="group relative">
-                <Button variant="outline" className="flex items-center group-hover:opacity-0">
-                  <AccountIcon address={account.address} size={20} />
-                  <div className="ml-2">
-                    {formatLongText(account.address, { headTailLength: 4 })}
-                  </div>
-                </Button>
-                <Button
-                  variant="destructive"
-                  className="absolute inset-0 flex h-auto items-center opacity-0 group-hover:opacity-100"
-                  onClick={() => disconnect(wagmiConfig)}
-                >
-                  Disconnect
-                </Button>
-              </div>
-            ) : (
-              <Button variant="outline" onClick={openConnectModal}>
-                Connect wallet
-              </Button>
-            )}
-          </div>
-        );
-      }}
-    </ConnectButton.Custom>
+  const { openConnectModal } = useConnectModal();
+
+  return (
+    <div className={cn('inline-flex space-x-4', className)} {...props}>
+      {account != null ? (
+        <div className="group relative">
+          <Button variant="outline" className="flex items-center group-hover:opacity-0">
+            <AccountIcon address={account} size={20} />
+            <div className="ml-2">{formatLongText(account, { headTailLength: 4 })}</div>
+          </Button>
+          <Button
+            variant="destructive"
+            className="absolute inset-0 flex h-auto items-center opacity-0 group-hover:opacity-100"
+            onClick={() => disconnect(wagmiConfig)}
+          >
+            Disconnect
+          </Button>
+        </div>
+      ) : (
+        <Button variant="outline" onClick={openConnectModal}>
+          Connect wallet
+        </Button>
+      )}
+    </div>
   );
 };
